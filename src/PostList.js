5d444f296b06d0af3d8554150316da989bb0c845
Qasim Laghari <go.jets.laghari@gmail.com>
2020-05-07 16:07:24 -0700

    Add state to post lists

--- src/PostList.js
+++ src/PostList.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 export default () => {
+    const [posts, setPosts] = useState({});
+
+    const fetchPosts = async () => {
+       const res = await axios.get('http://localhost:4000/posts');
+       setPosts(res.data); 
+    }
+
+    useEffect( ()=> {
+        fetchPosts();
+    },[]);
+
     return <div />;
 }
