a77ad4625a66000b1bb07ae880f833481546f663
Qasim Laghari <go.jets.laghari@gmail.com>
2020-05-07 16:28:44 -0700

    Fetch comments for a given post

--- src/CommentCreate.js
+++ src/CommentCreate.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 export default (props) => {
+    const [content, setContent] = useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post(`http://localhost:4001/posts/${props.postId}/comments`, {
+            content
+        });
+
+        setContent('');
+    };
+    
     return <div>
-        <form>
+        <form onSubmit={onSubmit} >
             <div className="form-group">
                 <label>New Comment</label>
-                <input className="form-control" />
+                <input 
+                value={content} 
+                onChange={e => setContent(e.target.value)}
+                className="form-control" />
             </div>
             <button className="btn btn-primary">Submit</button>
         </form>
