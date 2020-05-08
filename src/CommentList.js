59292a0dba48eaf4ad8a14effcc3e0e513308644
Qasim Laghari <go.jets.laghari@gmail.com>
2020-05-08 15:15:38 -0700

    Add comments list component

---
+++ src/CommentList.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+export default (props.postId) => {
+    //Initialize state for data
+    const [comments, setComments] = useState([]);
+    //Fetch Data
+    const fetchData = async () => {
+        const res = await axios.get(`http://localhost:4001/posts/${props.postId}/comments`);
+        //Set data on state
+        setComments(res.data);
+    }
+
+    //Fetch data on component load
+    useEffect( ()=> {
+        fetchData();
+    },[]);
+
+
+    const renderedComments = comments.map(comment => {
+        return <li key={comment.id}>{comment.content}</li>
+    });
+
+    return <ul>{renderedComments}</ul>;
+}
